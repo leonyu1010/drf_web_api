e3a977f593a3b3010d5020a5bef48b5d73553e5d
lyu1 <leon.yu@illumina.com>
2023-09-15 11:11:37 +0800

    feat: add CORS_ORIGIN_WHITELIST and CSRF_TRUSTED_ORIGINS

--- web_api/settings.py
+++ web_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
 
     'accounts.apps.AccountsConfig',
     'posts.apps.PostsConfig',
@@ -59,6 +60,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    "http://localhost:3000",
+    "http://localhost:8000",
+)
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
 
 ROOT_URLCONF = 'web_api.urls'
 
