44f41e463301058ea133efb08dcf88c41c893446
lyu1 <leon.yu@illumina.com>
2023-09-15 10:42:14 +0800

    feat: add new apps

---
+++ README.md
@@ -0,0 +1,1 @@
+# Web API with django 4.2 and django rest framework
--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BlogsConfig(AppConfig):
+class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blogs'
+    name = 'accounts'
